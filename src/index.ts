0e93326958fc29693017e2f0f7c67ecf5f9353f7
Ondřej Šúkala <xmbvztcxzc@email.cz>
2020-03-31 18:51:13 +0200

    changed return type of get method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 interface Array<T> {
-    get(index: number): T;
+    get(index: number): T | undefined;
     set(index: number, element: T): T[];
     remove(index: number): T[];
     delete(index: number): T[];
@@ -13,13 +13,16 @@ interface Array<T> {
 }
 
 if (!Array.prototype.get) {
-    Array.prototype.get = function<T>(this: T[], index: number): T | undefined {
+    Array.prototype.get = function <T>(
+        this: T[],
+        index: number
+    ): T | undefined {
         return this[index];
     };
 }
 
 if (!Array.prototype.set) {
-    Array.prototype.set = function<T>(
+    Array.prototype.set = function <T>(
         this: T[],
         index: number,
         element: T
@@ -31,13 +34,13 @@ if (!Array.prototype.set) {
 }
 
 if (!Array.prototype.remove) {
-    Array.prototype.remove = function<T>(this: T[], index: number): T[] {
+    Array.prototype.remove = function <T>(this: T[], index: number): T[] {
         return this.splice(index, 1);
     };
 }
 
 if (!Array.prototype.delete) {
-    Array.prototype.delete = function<T>(this: T[], index: number): T[] {
+    Array.prototype.delete = function <T>(this: T[], index: number): T[] {
         this.splice(index, 1);
 
         return this;
@@ -45,31 +48,31 @@ if (!Array.prototype.delete) {
 }
 
 if (!Array.prototype.isEmpty) {
-    Array.prototype.isEmpty = function<T>(this: T[]): boolean {
+    Array.prototype.isEmpty = function <T>(this: T[]): boolean {
         return this.length === 0;
     };
 }
 
 if (!Array.prototype.isNotEmpty) {
-    Array.prototype.isNotEmpty = function<T>(this: T[]): boolean {
+    Array.prototype.isNotEmpty = function <T>(this: T[]): boolean {
         return !this.isEmpty();
     };
 }
 
 if (!Array.prototype.clear) {
-    Array.prototype.clear = function<T>(this: T[]): void {
+    Array.prototype.clear = function <T>(this: T[]): void {
         this.length = 0;
     };
 }
 
 if (!Array.prototype.clone) {
-    Array.prototype.clone = function<T>(this: T[]): T[] {
+    Array.prototype.clone = function <T>(this: T[]): T[] {
         return this.slice(0);
     };
 }
 
 if (!Array.prototype.sum) {
-    Array.prototype.sum = function(this: number[]): number {
+    Array.prototype.sum = function (this: number[]): number {
         return this.reduce(
             (partialSum: number, element: number) => partialSum + element
         );
@@ -77,13 +80,13 @@ if (!Array.prototype.sum) {
 }
 
 if (!Array.prototype.min) {
-    Array.prototype.min = function(this: number[]): number {
+    Array.prototype.min = function (this: number[]): number {
         return Math.min.apply(null, this);
     };
 }
 
 if (!Array.prototype.max) {
-    Array.prototype.max = function(this: number[]): number {
+    Array.prototype.max = function (this: number[]): number {
         return Math.max.apply(null, this);
     };
 }
